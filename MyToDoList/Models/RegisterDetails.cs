4668d816418c81e23b3191350ab4e6d357d693cc
Dell <jaskiratkaur329@gmail.com>
2021-12-14 23:21:41 -0600

    Add project files.

---
+++ MyToDoList/Models/RegisterDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyToDoList.Models
+{
+    public class RegisterDetails
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Address { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime? DoB { get; set; }      
+        
+        
+    }
+}
